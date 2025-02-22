d981a104783ddae0db02e157fdd1b7000196f1c4
github_Neo0724 <neoben007@gmail.com>
2025-02-22 15:02:31 +0800

    Removed userid prop for all-post

--- app/post/all-posts/page.tsx
+++ app/post/all-posts/page.tsx
@@ -2,17 +2,11 @@
 
 import RenderPost from "../_component/postComponent/RenderPost";
 import { SearchPostType } from "../_component/Enum";
-import { useLocalStorage } from "@uidotdev/usehooks";
 
 export default function AllPostsPage() {
-  const [userId] = useLocalStorage<string | null>("test-userId");
-
   return (
     <>
-      <RenderPost
-        searchPostType={SearchPostType.ALL_POST}
-        userId={userId ?? ""}
-      />
+      <RenderPost searchPostType={SearchPostType.ALL_POST} />
     </>
   );
 }
